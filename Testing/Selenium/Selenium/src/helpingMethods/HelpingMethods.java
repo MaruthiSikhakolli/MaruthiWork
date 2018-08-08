4eb8b40f1c4e3ac91793bbd3d7b55f59c2487704
MaruthiSikhakolli <maruthismith@gmail.com>
2018-08-08 21:29:40 +0530

    Commiting latest

--- Testing/Selenium/Selenium/src/helpingMethods/HelpingMethods.java
+++ Testing/Selenium/Selenium/src/helpingMethods/HelpingMethods.java
@@ -8,6 +8,12 @@ import java.util.Iterator;
 import java.util.List;
 import java.util.Properties;
 
+import  java.sql.Connection;		
+import  java.sql.Statement;		
+import  java.sql.ResultSet;		
+import  java.sql.DriverManager;		
+import  java.sql.SQLException;	
+
 import javax.mail.BodyPart;
 import javax.mail.Message;
 import javax.mail.MessagingException;
@@ -146,4 +152,37 @@ public class HelpingMethods {
 
 		}
 	}
+	
+	//Method to connect to DB and fetch the data
+	public void SQLConnector() throws ClassNotFoundException, SQLException {
+		String dbUrl = "jdbc:mysql://localhost:3036/emp";
+		String username = "root";
+		String password = "guru99";
+
+		// Query to Execute
+		String query = "select *  from employee;";
+
+		// Load mysql jdbc driver
+		Class.forName("com.mysql.jdbc.Driver");
+
+		// Create Connection to DB
+		Connection con = DriverManager.getConnection(dbUrl, username, password);
+
+		// Create Statement Object
+		Statement stmt = con.createStatement();
+
+		// Execute the SQL Query. Store results in ResultSet
+		ResultSet rs = stmt.executeQuery(query);
+
+		// While Loop to iterate through all data and print results
+		while (rs.next()) {
+			String myName = rs.getString(1);
+			String myAge = rs.getString(2);
+			System.out.println(myName + "  " + myAge);
+		}
+
+		// closing DB Connection
+		con.close();
+	}
+	
 }
