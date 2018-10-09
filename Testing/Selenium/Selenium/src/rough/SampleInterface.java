8f73df33961a4cada5b97f49b4c6405905147efd
Maruthi <Ruthi@192.168.0.3>
2018-10-09 09:10:49 +0530

    Committing latest changes

---
+++ Testing/Selenium/Selenium/src/rough/SampleInterface.java
@@ -0,0 +1,7 @@
+package rough;
+
+public interface SampleInterface {
+	// This is to check whether we can declare variables without initializing them.
+	// int i; - This will thrown an error saying the variable i may not have been initialized.
+	int i = 20; // This is correct
+}
